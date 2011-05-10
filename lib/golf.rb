89ed86b4c7a8030188fe5e93f9d70f6762bad11c
Paul Battley <pbattley@gmail.com>
2011-05-10 07:26:51 +0100

    682

--- lib/golf.rb
+++ lib/golf.rb
@@ -1,50 +1,34 @@
-class<<Golf={}
-  def hole1 a
+Golf={}
+  %q^
     a.inject(1){|b,c|b*c}
-  end
 
-  def hole2 a
     a.split.sort_by{|w|w[1,1]}*" "
-  end
 
-  def hole3 a
     hole1 2..a
-  end
 
-  def hole4 a
     a.map{ |b|
       b =~ /man/ ? "hat(#{b})" : (b =~ /cat(.*)/ ? "dead#{$1}" : (b[")"] = "(bone))"
       b))
     }
-  end
 
-  def hole5 a
     (0...c=a.length).map{|b|
       (1..c-b).map{|d|
         a[b,d]
       }
     }.flatten(1).sort_by{|x|[x.length,x]}
-  end
 
-  def hole6 a
     (1..a).map{|b|
       c = [b%3<1?:fizz: "",b%5<1?:buzz: ""]*""
       c == "" ? b : c
     }
-  end
 
-  def hole7 a
     a.inject([]){|b,c|
       (b[-1]||[])[-1] == (c-1) ? b[-1] << c : b << [c]
       b
     }.map{|x|[x[0],x[-1]].uniq*"-"}
-  end
 
-  def hole8 a
     (1...a).inject([1]){|b,c| b << b[-1]+(b[-2]||0) }
-  end
 
-  def hole9 a
     b = File.readlines(a).map{|b|b.scan /\w+/}
     loop{
       e = b.inject({}){|h,c|d=c[0] or next
@@ -56,5 +40,6 @@ class<<Golf={}
       return e[-1][0] if f <= e[-1][1]
       b.map!{|x|x-[e[0][0]]}
     }
-  end
-end
+  ^.split(/\n\n/).each_with_index{|a,i| eval "def Golf.hole#{i+1} a
+"+a+"
+end"}
